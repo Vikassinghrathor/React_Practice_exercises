4f2cbd6fc6f814e243ebb0930b8740591c643aa3
vikassinghrathor <rathorevikas2809@gmail.com>
2024-06-09 19:10:09 +0530

    redux practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from "react";
-import AddContacts from "./components/AddContacts";
-import ContactList from "./components/ContactList";
-import Header from "./components/Header";
-
 const App = () => {
-  const [contact, setContact] = useState([]);
-
-  const addcontact = (data) => {
-    setContact(prevContacts => [...prevContacts, data]);
-  };
-
   return (
     <>
-      <Header />
-      <AddContacts addcontact={addcontact} />
-      <ContactList contact={contact} />
+        
     </>
-  );
+  )
 }
 
-export default App;
+export default App
