b9314d5b2e10c351fdb558fb259bcd55c4728d61
vikassinghrathor <rathorevikas2809@gmail.com>
2024-07-06 18:04:00 +0530

    increase count number using input value

--- src/components/ReduxCounter.jsx
+++ src/components/ReduxCounter.jsx
@@ -1,15 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "../redux/slices/appSlice";
+import { increaseCounter, decreaseCounter, updateNumber } from "../redux/slices/appSlice";
 
 const ReduxCounter = () => {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.appReducer.count);
+  const number = useSelector((state) => state.appReducer.number);
+
 
   return (
     <>
       <h1>Counter App using Redux</h1>
       <div className="counter">
         <p>Count: {count}</p>
+        <input value={number} type="text" placeholder="number count" onChange={(e) => dispatch(updateNumber(e.target.value))}/>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
       </div>
--- src/redux/slices/appSlice.ts
+++ src/redux/slices/appSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   count: 0,
+  number: 1,
 };
 
 const appSlice = createSlice({
@@ -9,14 +10,17 @@ const appSlice = createSlice({
   initialState,  // Use the defined initialState here
   reducers: {
     increaseCounter : (state) => {
-      state.count += 1;
+      state.count += Number(state.number);
     },
     decreaseCounter : (state) => {
-      state.count -= 1;
+      state.count -= Number(state.number);
     },
+    updateNumber: (state , action) => {
+      state.number = action.payload; // Update the number with the provided payload.
+    }
   },
 });
 
-export const { increaseCounter, decreaseCounter } = appSlice.actions; // Export the actions for use in other parts of the application.
+export const { increaseCounter, decreaseCounter , updateNumber} = appSlice.actions; // Export the actions for use in other parts of the application.
 
 export default appSlice.reducer;
